0ac1c4c16b56ea849c9b862919feb0db6fac5579
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-03-23 10:03:57 -0300

    Add project files.

---
+++ benchmark.dapper.async/DapperAsyncMethodBenchMark.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using System.Threading.Tasks;
+
+namespace benchmark.dapper.async
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+    public class DapperAsyncMethodBenchMark
+    {
+        [Benchmark]
+        public async Task GetAsync()
+        {
+            using (var conexao = new NpgsqlConnection("User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=sgp_db;Pooling=true"))
+            {
+                await conexao.QueryFirstOrDefaultAsync("select * from evento"); 
+            }
+            
+        }
+        [Benchmark]
+        public  void Get()
+        {
+            using (var conexao = new NpgsqlConnection("User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=sgp_db;Pooling=true"))
+            {
+                 conexao.QueryFirstOrDefault("select * from evento");
+            }
+
+        }
+    }
+}
---
+++ benchmark.dapper.async/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace benchmark.dapper.async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<DapperAsyncMethodBenchMark>();
+        }
+    }
+}
